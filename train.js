5a8d7af9c534a36cc1d915c0afb3d247d2c02937
Isa <theonewhohasthisemail@gmail.com>
2023-10-22 20:27:13 +0000

    allow the network to be saved to json

--- train.js
+++ train.js
@@ -22,5 +22,23 @@ try {
 
 network.train(data, {log: detail => console.log(detail)});
 
+try {
+  if (!fs.existsSync('./network')) {
+    fs.mkdirSync('./network');
+  } else {
+    let dataCount = 1
+    while (true) {
+      if (!fs.existsSync(`./network/${dataCount}.json`)) {
+        break
+      } else {
+        dataCount++
+      }
+    }
+    fs.writeFileSync("./network/"+dataCount+".json", JSON.stringify(network))
+  }
+} catch (err) {
+  console.error(err);
+}
+
 let result = network.run(strManipulation.translate("hi"))
 console.log(result)
