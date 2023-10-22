416c842e09b5bfe13af1f4df7ecdf4d3bc31acce
Isa <theonewhohasthisemail@gmail.com>
2023-10-22 20:23:29 +0000

    remove junk

--- train.js
+++ train.js
@@ -21,26 +21,6 @@ try {
 }
 
 network.train(data, {log: detail => console.log(detail)});
-/*
 
-network.train([
-  {input:strManipulation.translate("Hi Jack"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Hi Bob"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Hi Jill"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Hi Jenny"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Hi Alfred"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("bob"), output:{greeting:0}},
-  {input:strManipulation.translate("alfred"), output:{greeting:0}},
-  {input:strManipulation.translate("john"), output:{greeting:0}},
-  {input:strManipulation.translate("jill"), output:{greeting:0}},
-  {input:strManipulation.translate("hi"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Hi"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Hello"), output:{greeting:1, happy:1}},
-  {input:strManipulation.translate("Egg"), output:{greeting:0}},
-  {input:strManipulation.translate("waffles"), output:{greeting:0}},
-  {input:strManipulation.translate("Good day"), output:{greeting:1, happy:1}},
-
-], {log: detail => console.log(detail)});
-*/
 let result = network.run(strManipulation.translate("hi"))
 console.log(result)
