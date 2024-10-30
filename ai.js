7020ce9d0f29d99fc1594853b13b456a67767aa0
nottisa <theonewhohasthisemail@gmail.com>
2024-10-30 08:34:11 -0400

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,5 +1,5 @@
-dataSave = require('./modules/data');
-var strManipulation = require('./modules/stringManipulation');
+const dataSave = require('./modules/data');
+const strManipulation = require('./modules/stringManipulation');
 const network = dataSave.loadModel(3)
 
 console.log(network.run(strManipulation.translate("hi")))
